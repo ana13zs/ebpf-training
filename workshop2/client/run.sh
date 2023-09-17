c782169f796e41f7aaac120a8e9989b391f2f17f
ana13zs <ana_zafra_96@hotmail.com>
2023-09-17 18:32:28 +0200

    Modified workshops folder names

--- workshop2/client/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ping localhost -c 2 && ping localhost -6 -c 2
--- workshop3/cmd/sniffer/clean.c
+++
@@ -1,6 +0,0 @@
-// +build ignore
-
-#include <linux/in6.h>
-#include <linux/net.h>
-#include <linux/socket.h>
-#include <net/inet_sock.h>
